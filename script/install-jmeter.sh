41821821aaa03637133dcdbd06227bf3d0cd986d
dev-hippo-an <dkstpgud@gmail.com>
2024-03-14 09:20:36 +0900

    Write jmeter installation shell script

--- script/install-jmeter.sh
+++ script/install-jmeter.sh
@@ -1,64 +1,40 @@
 #!/bin/bash
-PROJECT_ROOT=$(pwd)
-JMETER_WORK_DIR="./third_party/jmeter"
-JMETER_VERSION="apache-jmeter-5.3"
-JMETER_INSTALL_URL="https://archive.apache.org/dist/jmeter/binaries/$JMETER_VERSION.tgz"
-JMETER_BIN="$JMETER_VERSION/bin"
+set -e
 
-# base setup
+# Base setup
+JMETER_WORK_DIR="/opt/jmeter"
+JMETER_VERSION=${JMETER_VERSION:="5.3"}
+JMETER_FOLDER="apache-jmeter-${JMETER_VERSION}"
+JMETER_FULL_PATH="${JMETER_WORK_DIR}/${JMETER_FOLDER}"
+JMETER_INSTALL_URL="https://archive.apache.org/dist/jmeter/binaries/$JMETER_FOLDER.tgz"
+
 mkdir -p $JMETER_WORK_DIR
 cd $JMETER_WORK_DIR
 
-# install need tools
-apt-get install -y wget default-jre
+# Installation
+apk upgrade
+apk add --update --no-cache openjdk11
 
 unzip_jmeter() {
-    tar xzvf "$JMETER_VERSION.tgz" && rm "$JMETER_VERSION.tgz"
-    rm -rf "$JMETER_VERSION/docs" "$JMETER_VERSION/printable_docs"
+  tar xzvf "$JMETER_FULL_PATH.tgz" && rm "$JMETER_FULL_PATH.tgz"
+  # delete unnecessary files
+  rm -rf "$JMETER_FULL_PATH/docs" "$JMETER_FULL_PATH/printable_docs"
 }
 
 # install jmeter
-if [ -d "$JMETER_VERSION" ]; then
-    echo "[CB-ANT] Jmeter is already installed."
-elif [ -f "$JMETER_VERSION.tgz" ]; then 
-    echo "[CB-ANT] Jmeter gzip file is installed on $JMETER_WORK_DIR. Let's do remaining installation."
-    unzip_jmeter
+if [ -d "$JMETER_FULL_PATH" ]; then
+  # jmeter folder already has
+  echo "[CB-ANT] Jmeter is already installed."
+elif [ -f "$JMETER_FULL_PATH.tgz" ]; then
+  # only jmeter tgz file has
+  echo "[CB-ANT] Jmeter gzip file is installed on $JMETER_WORK_DIR. Let's do remaining installation."
+  unzip_jmeter
 else
-    echo "[CB-ANT] JMeter is installing on path $JMETER_WORK_DIR"
-    wget $JMETER_INSTALL_URL
-    unzip_jmeter
+  # jmeter is not installed
+  echo "[CB-ANT] JMeter is installing on path $JMETER_WORK_DIR"
+  wget $JMETER_INSTALL_URL
+  unzip_jmeter
 fi
 
-
-# install cmd runner
-CMD_RUNNER_VERSION="2.2.1"
-CMD_RUNNER_JAR="cmdrunner-$CMD_RUNNER_VERSION.jar"
-
-if [ ! -e "$CMD_RUNNER_JAR" ]; then 
-    wget "https://repo1.maven.org/maven2/kg/apc/cmdrunner/$CMD_RUNNER_VERSION/$CMD_RUNNER_JAR"
-    
-    echo "[CB-ANT] Installed cmd runner."
-fi
-
-
-# install plugin manager
-PLUGIN_MANAGER_VERSION="1.6"
-PLUGIN_MANAGER_JAR="jmeter-plugins-manager-$PLUGIN_MANAGER_VERSION.jar"
-
-if [ ! -e "$PLUGIN_MANAGER_JAR" ]; then 
-    wget "https://repo1.maven.org/maven2/kg/apc/jmeter-plugins-manager/$PLUGIN_MANAGER_VERSION/$PLUGIN_MANAGER_JAR"
-    
-    echo "[CB-ANT] Installed plugin manager."
-fi
-
-cp $CMD_RUNNER_JAR "$JMETER_VERSION/lib/"
-cp $PLUGIN_MANAGER_JAR "$JMETER_VERSION/lib/ext/"
-
-# install perfmon plugin
-java -jar "$JMETER_VERSION/lib/$CMD_RUNNER_JAR" --tool org.jmeterplugins.repository.PluginManagerCMD install jpgc-perfmon,jpgc-dummy
-echo "[CB-ANT] Installed plugin perfmon."
-
-"$JMETER_BIN/jmeter" --version
-
-cd $PROJECT_ROOT
+sh "$JMETER_FULL_PATH/bin/jmeter.sh" --version
 echo "[CB-ANT] Jmeter is completely installed!!"
