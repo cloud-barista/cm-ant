a84bfbff920515c567ebe572fe023b6f5e14e4ef
hippo-an <dkstpgud@gmail.com>
2024-11-04 16:07:38 +0900

    optimize shell for install and remove agent and load generator

--- script/uninstall-jmeter.sh
+++ script/uninstall-jmeter.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-echo "[CM-ANT] JMeter Uninstallation"
+echo "[CM-ANT] Uninstalling JMeter..."
 set -e
 
-# Base setup
-JMETER_WORK_DIR=${JMETER_WORK_DIR:="/opt/ant/jmeter"}
-JMETER_VERSION=${JMETER_VERSION:="5.6"}
-JMETER_FOLDER="apache-jmeter-${JMETER_VERSION}"
-JMETER_FULL_PATH="${JMETER_WORK_DIR}/${JMETER_FOLDER}"
+# Default paths
+JMETER_WORK_DIR="${JMETER_WORK_DIR:=/opt/ant/jmeter}"
+JMETER_VERSION="${JMETER_VERSION:=5.6}"
+JMETER_PATH="${JMETER_WORK_DIR}/apache-jmeter-${JMETER_VERSION}"
 
-
-sudo rm -rf "${JMETER_FULL_PATH}"
-echo "[CM-ANT] Jmeter is completely uninstalled on system."
+# Remove JMeter directory
+sudo rm -rf "$JMETER_PATH"
+echo "[CM-ANT] JMeter uninstalled."
